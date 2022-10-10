3bb93e19786f8ea0b0e6a0a2c1c492240398dceb
Robin Hood <yoga3911@gmail.com>
2022-10-10 10:42:02 +0700

    p

---
+++ pertemuan 6/index.php
@@ -0,0 +1,48 @@
+<?php
+
+// $nama = "Mamat";
+// $umur = -20;
+
+// $data = printf("Nama saya adalah %s dan umur saya %u tahun", $nama, $umur);
+// echo $data;
+// $format = sprintf("Nama saya adalah %s dan umur saya %u tahun", $nama, $umur);
+
+// echo str_pad("Kucing lucu", 20, "=", STR_PAD_LEFT);
+// echo "<br>";
+// echo str_pad("Kucing lucu", 20, "=", STR_PAD_RIGHT);
+// echo "<br>";
+// echo str_pad("Kucing lucu", 20, "=", STR_PAD_BOTH);
+// echo "<br>";
+
+// echo time();
+// echo date("l F Y");
+
+// Localization
+// $tanggal = mktime(10, 50, 10, 12, 12, 2022);
+// $tanggal = strtotime("Kamis April 2022");
+// echo date("l F Y, H:i:s", $tanggal);
+// echo date_default_timezone_get();
+// echo date_default_timezone_set("Asia/Jakarta");
+// echo date_default_timezone_get();
+
+// Pembulatan
+// floor();
+// ceil();
+// round();
+
+// $uang = 100000.155555;
+// echo number_format($uang, 5);
+
+// echo exec("whoami");
+// system("");
+
+// readfile("data.txt");
+// Read
+// $data = fopen("data.txt", "r");
+// $data = fopen("data.txt", "w");
+// $data = fopen("data.txt", "a");
+// echo fread($data, filesize("data.txt"));
+// fwrite($data, "Po\n");
+// echo fread($data, filesize("data.txt"));
+// fclose($data);
+
