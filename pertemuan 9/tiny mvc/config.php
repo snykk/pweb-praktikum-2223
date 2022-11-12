ec5de89b61d641b3badfabdb2672a22452a87d8d
Robin Hood <yoga3911@gmail.com>
2022-11-12 12:40:30 +0700

    fix random warning

--- pertemuan 9/tiny mvc/config.php
+++ pertemuan 9/tiny mvc/config.php
@@ -1,5 +1,5 @@
 <?php
-define('PATH_ROOT', __DIR__ . '/');
-define('PATH_CONTROLLERS', PATH_ROOT . 'controllers/');
-define('PATH_MODELS', PATH_ROOT . 'models/');
-define('PATH_VIEWS', PATH_ROOT . 'views/');
+if (!defined('PATH_ROOT')) define('PATH_ROOT', __DIR__ . '/');
+if (!defined('PATH_CONTROLLERS')) define('PATH_CONTROLLERS', PATH_ROOT . 'controllers/');
+if (!defined('PATH_MODELS')) define('PATH_MODELS', PATH_ROOT . 'models/');
+if (!defined('PATH_VIEWS')) define('PATH_VIEWS', PATH_ROOT . 'views/');
